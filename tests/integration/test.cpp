b2684d68da14791c87c49de3b18c25ae5df6ef32
Daniel Rakos <daniel.rakos@rastergrid.com>
2024-05-23 17:06:21 +0200

    Add install target and integration tests

---
+++ tests/integration/test.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 The Khronos Group Inc.
+ * Copyright (c) 2024 RasterGrid Kft.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <pcreader.hpp>
+#include <pcwriter.hpp>
+
+int main() {
+    return 0;
+}
